d56e9168771238db0af0429cf9e77b356d846b4a
ManoharSankar <manoharsankar93@gmail.com>
2026-01-22 12:03:27 +0530

    Added the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-provider "aws"{
-    region = var.region
-}
-module "eks" {
-  source = "./modules/eks"
-
-  cluster_name    = var.cluster_name
-  private_subnets = var.private_subnets
-  vpc_id          = var.vpc_id
+provider "aws" {
+  region = var.region
 }
